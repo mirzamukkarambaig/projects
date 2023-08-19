5577714b98939c67e2ab1340ac0346eb7c466da6
Mirza Mukkaram Baig <chugtai15dec@outlook.com>
2023-08-19 15:09:36 +0500

    Folders updated & automated scripted added

---
+++ git_push.py
@@ -0,0 +1,28 @@
+import git
+from getpass import getpass
+
+def git_push(repo_path='.'):
+    # Initialize the repository
+    repo = git.Repo(repo_path)
+
+    # Check if there are any changes
+    if repo.is_dirty():
+        # Add all changes
+        repo.git.add(A=True)
+        
+        # Commit
+        commit_message = input("Enter commit message: ")
+        repo.index.commit(commit_message)
+
+        # Push to GitHub
+        try:
+            origin = repo.remote(name='origin')
+            origin.push()
+            print("Changes pushed to GitHub!")
+        except git.exc.GitCommandError as error:
+            print(f"Error pushing to GitHub: {error}")
+    else:
+        print("No changes detected.")
+
+if __name__ == "__main__":
+    git_push()
