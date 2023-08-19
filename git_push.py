25fd7adeff45d278e825da9b291919e897a8942e
Mirza Mukkaram Baig <chugtai15dec@outlook.com>
2023-08-19 15:19:47 +0500

    folders updated and automated script added

--- git_push.py
+++ git_push.py
@@ -1,9 +1,16 @@
 import git
 from getpass import getpass
 
-def git_push(repo_path='.'):
+def git_push(repo_path='.', remote_url='https://github.com/mirzamukkarambaig/projects.git'):
     # Initialize the repository
     repo = git.Repo(repo_path)
+    
+    # Set the origin URL
+    try:
+        origin = repo.remote(name='origin')
+        origin.set_url(remote_url)
+    except git.exc.NoSuchPathError:
+        repo.create_remote('origin', url=remote_url)
 
     # Check if there are any changes
     if repo.is_dirty():
@@ -16,7 +23,6 @@ def git_push(repo_path='.'):
 
         # Push to GitHub
         try:
-            origin = repo.remote(name='origin')
             origin.push(refspec='HEAD:main')  # Push to the main branch
             print("Changes pushed to GitHub!")
         except git.exc.GitCommandError as error:
