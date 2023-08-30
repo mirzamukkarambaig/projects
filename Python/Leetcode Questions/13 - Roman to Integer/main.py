c5f522099b53e097443d8b934c75260408bf60ac
Mirza Mukkaram Baig <chugtai15dec@outlook.com>
2023-08-31 03:25:07 +0500

    roman to int code improved

--- Python/Leetcode Questions/13 - Roman to Integer/main.py
+++ Python/Leetcode Questions/13 - Roman to Integer/main.py
@@ -1,14 +1,15 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         decoder = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        sum = 0
-        prev_value = 0  # Initialize a variable to keep track of the previous value
-        for i in range(len(s) - 1, -1, -1):
-            value = decoder.get(s[i])
-            # If the current value is smaller than the previous value, subtract it; otherwise, add it.
+        total = 0
+        prev_value = 0
+        
+        for char in reversed(s):
+            value = decoder[char]
             if value < prev_value:
-                sum -= value
+                total -= value
             else:
-                sum += value
-            prev_value = value  # Update the previous value for the next iteration
-        return sum
+                total += value
+            prev_value = value
+        
+        return total
