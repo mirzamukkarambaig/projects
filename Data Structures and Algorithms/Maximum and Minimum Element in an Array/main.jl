fecaa44815497ae932f3b00abf47079a27215cff
Mirza Mukkaram Baig <chugtai15dec@outlook.com>
2023-09-07 18:55:53 +0500

    Julia file debugged

--- Data Structures and Algorithms/Maximum and Minimum Element in an Array/main.jl
+++ Data Structures and Algorithms/Maximum and Minimum Element in an Array/main.jl
@@ -1,11 +1,12 @@
 # Initialize variables to hold maximum and minimum values
-current_maximum = typemin(Int32)  # Start with the smallest possible value
-current_minimum = typemax(Int32)  # Start with the largest possible value
+global current_maximum = typemin(Int32)  # Start with the smallest possible value
+global current_minimum = typemax(Int32)  # Start with the largest possible value
 
 my_list = [5, 15, 50, 3, 67, 100]
 
 # Loop through each element in my_list
 for i in my_list
+    global current_minimum, current_maximum  # Declare as global within the loop
     if current_minimum > i
         current_minimum = i
     end
