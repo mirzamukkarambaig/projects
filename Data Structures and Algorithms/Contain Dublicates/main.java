ea62afe3407d7d32b7da83e60ceece17ba460868
Mirza Mukkaram Baig <chugtai15dec@outlook.com>
2023-09-07 08:01:43 +0500

    DSA Contains dublicates C++

--- Data Structures and Algorithms/Contain Dublicates/main.java
+++ Data Structures and Algorithms/Contain Dublicates/main.java
@@ -1,19 +1,21 @@
 import java.util.HashSet;
 import java.util.Set;
 
-public class Main{
-    public static void main(String[] args){
+public class Main {
+    public static void main(String[] args) {
         int[] myArray = {1, 2, 3};
-        Set<String> mySet = new HashSet<>();
-        boolean dublicates = false;
+        Set<Integer> mySet = new HashSet<>();  // Changed type to Integer
+        boolean duplicates = false;  // Corrected the spelling
 
-        for (int num : myArray){
-            if (mySet.contains(num)){
-                dublicates = !dublicates;
-            }
-            else{
-                mySet.add(num)
+        for (int num : myArray) {
+            if (mySet.contains(num)) {
+                duplicates = true;  // Set to true directly
+                break;  // Exit the loop as we've found a duplicate
+            } else {
+                mySet.add(num);
             }
         }
+
+        System.out.println("Duplicates found: " + duplicates);
     }
 }
