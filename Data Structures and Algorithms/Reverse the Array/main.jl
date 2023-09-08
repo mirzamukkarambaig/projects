9d8b59d36542809114ab3e5d5537942a6c1c063c
Mirza Mukkaram Baig <chugtai15dec@outlook.com>
2023-09-08 20:51:43 +0500

    JS file added

--- Data Structures and Algorithms/Reverse the Array/main.jl
+++ Data Structures and Algorithms/Reverse the Array/main.jl
@@ -1,7 +1,7 @@
 my_list = [1, 2, 3]
 
 for i in 1:div(length(my_list), 2)
-    j = length(my_list) - 1 - i
+    j = length(my_list) + 1 - i
 
     my_list[i], my_list[j] = my_list[j], my_list[i]
 end
