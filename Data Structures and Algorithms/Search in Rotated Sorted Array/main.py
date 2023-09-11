cb2e480d004722beac100e0648e9463448f296ae
Mirza Mukkaram Baig <chugtai15dec@outlook.com>
2023-09-11 15:44:03 +0500

    binary search

--- Data Structures and Algorithms/Search in Rotated Sorted Array/main.py
+++ Data Structures and Algorithms/Search in Rotated Sorted Array/main.py
@@ -1,14 +1,28 @@
+# element_to_find = 0
+
+# if element_to_find in my_list:
+#     index = my_list.index(element_to_find)
+#     print(f"The element {element_to_find} is at index {index}.")
+# else:
+#     print(f"The element {element_to_find} is not in the list.")
+
+def binary_search(list, key):
+    start = 0
+    end = len(list) - 1
+
+    while(start <= end):
+        mid = int((start + end) / 2)
+
+        if(list[mid] == key):
+            return mid
+        elif(list[mid]>key):
+            end = mid - 1
+        else:
+            start = mid + 1
+
+    return -1
+
+
 my_list = [0,1,2,4,5,6,7]
-n = 3
-
-for i in range(n):
-    element = my_list.pop(0)
-    my_list.append(element)
-
-element_to_find = 0
-
-if element_to_find in my_list:
-    index = my_list.index(element_to_find)
-    print(f"The element {element_to_find} is at index {index}.")
-else:
-    print(f"The element {element_to_find} is not in the list.")
+result = binary_search(my_list, 2)
+print(result)
