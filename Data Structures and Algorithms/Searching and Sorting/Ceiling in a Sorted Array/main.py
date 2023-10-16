d7eeb81d238816b0aeeebc5754f69098a5e8e63f
Mirza Mukkaram Baig <chugtai15dec@outlook.com>
2023-10-17 03:01:25 +0500

    Function Update

--- Data Structures and Algorithms/Searching and Sorting/Ceiling in a Sorted Array/main.py
+++ Data Structures and Algorithms/Searching and Sorting/Ceiling in a Sorted Array/main.py
@@ -1,26 +1,43 @@
-def binary_search(list, key):
-    low = 0
-    high = len(list) - 1
+from typing import List
 
-    while(low <= high):
+from typing import List, Tuple
+
+def binary_search(values: List[int], key: int) -> Tuple[int, int]:
+    """
+    Performs a binary search for the given key in the sorted list.
+    If key is found, returns its index.
+    If key is not found, returns the indices where the key should be inserted (low and high).
+    """
+    low = 0
+    high = len(values) - 1
+
+    while low <= high:
         mid = (low + high) // 2
 
-        if(list[mid] == key):
+        if values[mid] == key:
             return mid
-        elif(list[mid]>key):
+        elif values[mid] > key:
             high = mid - 1
         else:
             low = mid + 1
 
     return low, high
-    
-def ceil(list: list, key) -> int:
-    low, high = binary_search(list, key)
-    return list[low]
 
-def floor(list: list, key) -> int:
-    low, high = binary_search(list, key)
-    return list[high]
+    
+def ceil(values: List[int], key: int) -> int:
+    """
+    Returns the ceiling value for the given key from the sorted list.
+    """
+    low, high = binary_search(values, key)
+    return values[low]
+
+def floor(values: List[int], key: int) -> int:
+    """
+    Returns the floor value for the given key from the sorted list.
+    """
+    low, high = binary_search(values, key)
+    return values[high]
+
 
 
 array = [1, 2, 8, 10, 10, 12, 19]
