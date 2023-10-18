1c185b562aef5a0b9306cfb5e4018eb412efd795
Mirza Mukkaram Baig <chugtai15dec@outlook.com>
2023-10-18 16:22:02 +0500

    Basic Linkedlist implementation

---
+++ Data Structures and Algorithms/Linked List/Linked List/main.py
@@ -0,0 +1,37 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Linkedlist:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def append(self, data):
+        temp = Node(data)
+        if not self.head:
+            self.head = temp
+            self.tail = temp
+        else:
+            self.tail.next = temp  
+            self.tail = temp
+
+    def print_list(self):
+        if self.head is None:
+            print("Linked list is empty")
+            return
+        cur_node = self.head
+        while cur_node is not None:
+            print(cur_node.data, end=" -> ")
+            cur_node = cur_node.next
+        print("None")
+        
+linkedlist = Linkedlist()
+
+linkedlist.append(1)
+linkedlist.append("Mukkaram")
+linkedlist.append(56.21)
+linkedlist.append(True)
+
+linkedlist.print_list()
