114cfc056932b0f662c88433a43f22a402ebeaa9
Mirza Mukkaram Baig <chugtai15dec@outlook.com>
2023-10-29 19:37:00 +0500

    Prefix Expression Evaluation Optimized

--- Data Structures and Algorithms/Stacks & Queues/Prefix Expression Evaluation/main.py
+++ Data Structures and Algorithms/Stacks & Queues/Prefix Expression Evaluation/main.py
@@ -1,35 +1,42 @@
-def prefix_evaluation(string: str) -> int:
+def evaluate_operator(operator, operand1, operand2):
+    mapping = {
+        "+": lambda x, y: x + y,
+        "-": lambda x, y: x - y,
+        "*": lambda x, y: x * y,
+        "/": lambda x, y: x / y if y != 0 else float('inf'),  # Handling division by zero
+        "^": lambda x, y: x ** y
+    }
+
+    if operator not in mapping:
+        raise ValueError(f"Unknown operator: {operator}")
+    
+    return mapping[operator](operand1, operand2)
+
+
+def prefix_evaluation(expression: str) -> int:
     stack = []
 
-    # Reverse the string and iterate over it
-    for char in reversed(string):
-        if '0' <= char <= '9':
-            stack.append(int(char))
+    # Splitting the string by whitespace to handle multi-digit numbers and operators
+    tokens = expression.split()
+
+    for token in reversed(tokens):
+        if token.isdigit():  # Using isdigit() to check for numbers
+            stack.append(int(token))
         else:
             # Ensuring there are enough operands in the stack
             if len(stack) < 2:
-                raise ValueError("Invalid prefix expression")
+                raise ValueError(f"Invalid prefix expression: insufficient operands for operator {token}")
 
             operand1 = stack.pop()
             operand2 = stack.pop()
 
-            mapping = {
-                "+": lambda: operand1 + operand2,
-                "-": lambda: operand1 - operand2,
-                "*": lambda: operand1 * operand2,
-                "/": lambda: operand1 / operand2,
-                "^": lambda: operand1 ** operand2  
-            }
-            
-            if char in mapping:
-                stack.append(mapping[char]())  
-            else:
-                raise ValueError(f"Unknown operator: {char}")
+            result = evaluate_operator(token, operand1, operand2)
+            stack.append(result)
 
     # If the evaluation is correct, only one value should remain
     if len(stack) != 1:
-        raise ValueError("Invalid prefix expression")
+        raise ValueError("Invalid prefix expression: imbalanced operators and operands")
     
     return stack.pop()
 
-print(prefix_evaluation("+-32*45"))
+print(prefix_evaluation("+ - 3 2 * 4 5"))
