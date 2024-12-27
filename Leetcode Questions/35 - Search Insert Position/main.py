23bb52fafd19651ab3e4f6da621b89c107f6be4f
mirzamukkarambaig <mirza.mukkaram.baig@outlook.com>
2024-12-27 13:24:02 +0500

    Leetcode Q.35

---
+++ Leetcode Questions/35 - Search Insert Position/main.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        i = 0 
+
+        while i < len(nums) and nums[i] < target:
+            i += 1
+
+        return i
